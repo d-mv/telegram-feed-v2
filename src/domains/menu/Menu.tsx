4bd206d62d8ed5c2c83381eefef51caabc1e96cf
Dmitry Melnikov <d.dash.mv@gmail.com>
2026-03-23 09:48:52 +0000

    Defer Telegram search panel loading

--- src/domains/menu/Menu.tsx
+++ src/domains/menu/Menu.tsx
@@ -1,13 +1,14 @@
 import { useAtom, useAtomValue, useSetAtom } from "jotai/react";
-import { Suspense, useContext } from "react";
+import { lazy, Suspense, useContext } from "react";
 import { menuIsOpenAtom, menuItemAtom, toggleMenuAtom } from "../../atoms/menu.atom";
 import { AppContext } from "../app/AppContext";
 import AvatarsSettings from "./components/AvatarsSettings";
 import FiltersSettings from "./components/FiltersSettings";
 import Maintenance from "./components/Maintenance";
 import NotificationsSettings from "./components/NotificationsSettings";
-import SearchDialog from "./components/SearchDialog";
 import styles from "./Menu.module.css";
+
+const SearchDialog = lazy(() => import("./components/SearchDialog"));
 
 const MENU_ITEMS = [
   {
